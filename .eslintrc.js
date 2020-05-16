de3e74b85fc44ad3f9529d535ec8329306e35d08
owlran <owlran1088@gmail.com>
2020-05-17 01:27:04 +0800

    chore: update rule of max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': [2, 300, 4, {"ignoreUrls": true}]
   },
   overrides: [
     {
