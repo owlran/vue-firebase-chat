c578c3e608c4710d372fc14036d5691e23a856c2
owlran <owlran1088@gmail.com>
2020-05-18 02:50:10 +0800

    refactor: add stitcker picker

---
+++ src/components/ImagePicker.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .imagePicker(class="grid grid-cols-3 gap-4 bg-white")
+    .imagePicker__grid(
+      v-for="(img, index) in imgSrc" :key="index"
+      @click="emitImg(index)"
+      )
+      img(:src="img")
+</template>
+
+<script>
+import stitcker1 from '@/assets/stitcker/sticker1.png';
+import stitcker2 from '@/assets/stitcker/sticker2.png';
+import stitcker3 from '@/assets/stitcker/sticker3.png';
+
+export default {
+  methods: {
+    emitImg(index) {
+      this.$emit('addStitcker', this.imgSrc[index]);
+    },
+  },
+  computed: {
+    imgSrc() {
+      return [stitcker1, stitcker2, stitcker3];
+    },
+    img1Src() {
+      return stitcker1;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.imagePicker {
+  &__grid {
+    @apply flex flex-row justify-center cursor-pointer;
+  }
+}
+</style>
--- src/components/Typer.vue
+++ src/components/Typer.vue
@@ -1,9 +1,13 @@
 <template lang='pug'>
-  .typer(class="relative bottom-0")
+  .typer(
+    :class="typeClassObj"
+    class="fixed bottom-0 flex flex-col w-full bg-white")
     .typer-wrapper(class="flex flex-row justify-around items-center h-20")
       .typer__functions(class="w-2/12 flex flex-row justify-around text-blue-900")
         font-awesome-icon(class="cursor-not-allowed" :icon="['far', 'image']" size="lg")
-        font-awesome-icon(class="cursor-pointer" :icon="['far', 'laugh']" size="lg")
+        font-awesome-icon(
+          @click="isImagePickerActived = !isImagePickerActived"
+          class="cursor-pointer" :icon="['far', 'laugh']" size="lg")
       .typer__inputField(class="w-8/12")
         input(
           v-model="chat"
@@ -15,16 +19,34 @@
           class="cursor-pointer"
           @click="sendMessage"
           :icon="['fas', 'arrow-right']" size="lg")
+    ImagePicker(@addStitcker="addStitcker")
 </template>
 
 <script>
+import ImagePicker from '@/components/ImagePicker.vue';
+
 export default {
   data() {
     return {
       chat: '',
+      isImagePickerActived: false,
     };
   },
+  computed: {
+    typeClassObj() {
+      return {
+        'typer--active': this.isImagePickerActived,
+      };
+    },
+  },
+  components: {
+    ImagePicker,
+  },
   methods: {
+    addStitcker(imgSrc) {
+      this.$emit('addStitcker', imgSrc);
+      this.isImagePickerActived = false;
+    },
     sendMessage() {
       this.$emit('sendMessage', this.chat);
       this.chat = '';
@@ -34,4 +56,12 @@ export default {
 </script>
 
 <style lang='scss'>
+.typer {
+  bottom: -235px;
+  transition: all .3s ease;
+
+  &--active {
+    bottom: 0;
+  }
+}
 </style>
