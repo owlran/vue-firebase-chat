f23a7c54908adf8f05c4fcb37e7732c7eccf236d
owlran <owlran1088@gmail.com>
2020-05-18 01:55:50 +0800

    refactor(Typer): add typer layout

--- src/components/Typer.vue
+++ src/components/Typer.vue
@@ -1,22 +1,36 @@
 <template lang='pug'>
-  .typer(class="mb-4")
-    label(class="block text-gray-700 text-sm font-bold mb-2" for="chat") chat
-    input(
-      v-model="chat"
-      class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      @keyup.enter="sendMessage"
-      id="chat"
-      type="text"
-      placeholder="chat")
-    button(class="border border-gray-600 p-2" @click="sendMessage") send
+  .typer(class="flex flex-row justify-around items-center mb-4")
+    .typer__functions(class="w-2/12 flex flex-row justify-around")
+      font-awesome-icon(class="cursor-not-allowed" :icon="['far', 'image']" size="lg")
+      font-awesome-icon(class="cursor-pointer" :icon="['far', 'laugh']" size="lg")
+    .typer__inputField(class="w-8/12")
+      input(
+        v-model="chat"
+        class="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        @keyup.enter="sendMessage"
+        type="text")
+    .typer__submit(class="w-2/12 flex flex-row justify-center items-center")
+      font-awesome-icon(
+        class="cursor-pointer"
+        @click="sendMessage"
+        :icon="['fas', 'arrow-right']" size="lg")
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      chat: '',
+    };
+  },
+  methods: {
+    sendMessage() {
+      this.$emit('sendMessage', this.chat);
+      this.chat = '';
+    },
+  },
 };
 </script>
 
 <style lang='scss'>
-
 </style>
