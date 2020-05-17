64374951198a30868a896aa9f8982b591356981b
owlran <owlran1088@gmail.com>
2020-05-18 00:18:52 +0800

    refactor(Chatroom): extract typer component

---
+++ src/components/Typer.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+  .typer(class="mb-4")
+    label(class="block text-gray-700 text-sm font-bold mb-2" for="chat") chat
+    input(
+      v-model="chat"
+      class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      @keyup.enter="sendMessage"
+      id="chat"
+      type="text"
+      placeholder="chat")
+    button(class="border border-gray-600 p-2" @click="sendMessage") send
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang='scss'>
+
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,20 +1,11 @@
 <template lang="pug">
   .chats
     ChatList(:chats="getChats")
-    div(class="mb-4")
-      label(class="block text-gray-700 text-sm font-bold mb-2" for="chat") chat
-      input(
-        v-model="chat"
-        class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        @keyup.enter="sendMessage"
-        id="chat"
-        type="text"
-        placeholder="chat")
-      button(class="border border-gray-600 p-2" @click="sendMessage") send
+    Typer
 </template>
 
 <script>
-// @ is an alias to /src
+import Typer from '@/components/Typer.vue';
 import ChatList from '@/components/ChatList.vue';
 import { mapGetters, mapActions } from 'vuex';
 
@@ -27,6 +18,7 @@ export default {
     };
   },
   components: {
+    Typer,
     ChatList,
   },
   computed: {
