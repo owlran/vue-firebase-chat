d60cff72dcaed952383a7ca14761f39589f4e6f3
owlran <owlran1088@gmail.com>
2020-05-18 01:54:59 +0800

    refactor: add fontawsome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faImage, faLaugh } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import './assets/main.css';
+
+library.add(faArrowRight, faImage, faLaugh);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
